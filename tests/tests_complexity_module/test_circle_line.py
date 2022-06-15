9fd2a108157c0a0bbbf406a8ca396263b2c3b68b
gordeev.al <aleksandr_gordeev2@epam.com>
2022-06-15 21:24:50 +0300

    added templates

---
+++ tests/tests_complexity_module/test_circle_line.py
@@ -0,0 +1,9 @@
+from algorithms.complexity_testing import circle_line
+
+
+def test_1():
+    print("hello, test")
+
+
+if __name__ == "__main__":
+    test_1()
---
+++ tests/tests_complexity_module/test_interactor.py
@@ -0,0 +1,9 @@
+from algorithms.complexity_testing import interactor
+
+
+def test_1():
+    print("hello, test")
+
+
+if __name__ == "__main__":
+    test_1()
