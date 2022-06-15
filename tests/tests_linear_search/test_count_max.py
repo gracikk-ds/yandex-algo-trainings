6e32360d86e7ca3ba4a299ccf87d4a847581e0af
gordeev.al <aleksandr_gordeev2@epam.com>
2022-06-15 21:26:20 +0300

    added description

---
+++ tests/tests_linear_search/test_count_max.py
@@ -0,0 +1,9 @@
+from algorithms.linear_search import count_max
+
+
+def test_1():
+    print("hello, test")
+
+
+if __name__ == "__main__":
+    test_1()
---
+++ tests/tests_linear_search/test_palindrome.py
@@ -0,0 +1,9 @@
+from algorithms.linear_search import palindrome
+
+
+def test_1():
+    print("hello, test")
+
+
+if __name__ == "__main__":
+    test_1()
