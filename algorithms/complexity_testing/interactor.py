0783b23f2282ccaa04f3232901b1f0a1f83910d6
gordeev.al <aleksandr_gordeev2@epam.com>
2022-06-22 17:25:12 +0300

    solved task number 1

---
+++ algorithms/complexity_testing/interactor.py
@@ -0,0 +1,81 @@
+from typing import Union, Any
+
+
+def interactor_verdict(
+        task_code=None,
+        interactor_result=None,
+        checker_result=None
+) -> Union[Union[str, int], Any]:
+    """
+    1. Interactor task
+    :param task_code: код завершения задачи
+    :param interactor_result: вердикт интерактора
+    :param checker_result: Вердикт чекера
+    :return: итоговый вердикт
+    """
+
+    if task_code is None:
+        print("Please, input task code:")
+        task_code = int(input())
+    else:
+        print(f"task_code value: {task_code}")
+
+    if interactor_result is None:
+        print("Please, input interactor result:")
+        interactor_result = int(input())
+    else:
+        print(f"interactor_result value: {interactor_result}")
+
+    if checker_result is None:
+        print("Please, input checker result:")
+        checker_result = int(input())
+    else:
+        print(f"checker_result value: {checker_result}")
+
+    if (127 < task_code) | (-127 > task_code):
+        print("TaskCode Error")
+        return "Input Error"
+    if (7 < interactor_result) | (0 > interactor_result):
+        print("InteractorResult Error")
+        return "Input Error"
+
+    if (7 < checker_result) | (0 > checker_result):
+        print("CheckerResult Error")
+        return "Input Error"
+
+    if interactor_result == 0:
+        if task_code != 0:
+            print(3)
+            return 3
+        else:
+            print(checker_result)
+            return checker_result
+
+    elif interactor_result == 1:
+        print(checker_result)
+        return checker_result
+
+    elif interactor_result == 4:
+        if task_code != 0:
+            print(3)
+            return 3
+        else:
+            print(4)
+            return 4
+
+    elif interactor_result == 6:
+        print(0)
+        return 0
+
+    elif interactor_result == 7:
+        print(1)
+        return 1
+
+    else:
+        return interactor_result
+
+
+if __name__ == "__main__":
+    result = interactor_verdict()
+    print(f"interactor_result: {result}")
+
--- tests/tests_complexity_module/test_interactor.py
+++ tests/tests_complexity_module/test_interactor.py
@@ -1,9 +1,26 @@
 from algorithms.complexity_testing import interactor
 
 
-def test_1():
-    print("hello, test")
+def test_interactor():
+    """
+    Test for interactor programme
+    :return: None
+    """
+    test_case_1 = [[0, 0, 0], 0]
+    test_case_2 = [[-1, 0, 1], 3]
+    test_case_3 = [[42, 1, 6], 6]
+    test_case_4 = [[44, 7, 4], 1]
+    test_case_5 = [[1, 4, 0], 3]
+    test_case_6 = [[-3, 2, 4], 2]
+    test_case_7 = [[3000, 3000, 3000], "Input Error"]
 
+    test_container = [
+        test_case_1, test_case_2, test_case_3,
+        test_case_4, test_case_5, test_case_6,
+        test_case_7,
+    ]
 
-if __name__ == "__main__":
-    test_1()
+    for test_case in test_container:
+        variables, result_target = test_case
+        result_program = interactor.interactor_verdict(*variables)
+        assert result_target == result_program
