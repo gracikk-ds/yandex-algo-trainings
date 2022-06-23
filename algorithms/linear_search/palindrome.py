fd3c9ca400c1b5f64bad58acf9bc339869fd96df
gordeev.al <aleksandr_gordeev2@epam.com>
2022-06-23 17:33:19 +0300

    solved task palindrome

---
+++ algorithms/linear_search/palindrome.py
@@ -0,0 +1,28 @@
+import math
+
+
+def palindrome(string=None) -> int:
+    """
+    Create palindrome from string
+    :param string: string to fix
+    :return: max distance
+    """
+    if string is None:
+        string = input()
+
+    string_lenth = len(string)
+
+    left_side = string[:math.floor(string_lenth / 2)]
+    right_side = string[math.ceil(string_lenth / 2):]
+
+    payment = 0
+    for i, j in zip(left_side, right_side[::-1]):
+        if i != j:
+            payment += 1
+
+    return payment
+
+
+if __name__ == "__main__":
+    result = palindrome()
+    print(result)
--- tests/tests_linear_search/test_palindrome.py
+++ tests/tests_linear_search/test_palindrome.py
@@ -1,9 +1,23 @@
 from algorithms.linear_search import palindrome
 
 
-def test_1():
-    print("hello, test")
+def test_palindrome():
+    """
+    Test for palindrome programme
+    :return: None
+    """
+    test_case_1 = ["a", 0]
+    test_case_2 = ["ab", 1]
+    test_case_3 = ["abc", 1]
+    test_case_4 = ["cognitive", 4]
+    test_case_5 = ["abcfcba", 0]
+    test_case_6 = ["abccba", 0]
 
+    test_container = [
+        test_case_1, test_case_2, test_case_3,
+        test_case_4, test_case_5, test_case_6]
 
-if __name__ == "__main__":
-    test_1()
+    for test_case in test_container:
+        variables, result_target = test_case
+        result_program = palindrome.palindrome(variables)
+        assert result_target == result_program
